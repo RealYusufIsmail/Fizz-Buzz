2991ea150ee4312198a7b544c0f9cb644014f387
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-08-21 14:08:14 +0100

    Update Main.java

--- src/main/java/io/github/realyusufismail/Main.java
+++ src/main/java/io/github/realyusufismail/Main.java
@@ -3,23 +3,14 @@ package io.github.realyusufismail;
 public class Main {
     public static void main(String[] args) {
 
-       for (int i = 0; i <= 100; i++) {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) System.out.println("FizzBuzz");
 
-          if (checkIfMultipleOfThree(i)) System.out.println("Fizz");
+            else if (i % 3 == 0) System.out.println("Fizz");
 
-          else if (checkIfMultipleOfFive(i)) System.out.println("Buzz");
+            else if (i % 5 == 0) System.out.println("Buzz");
 
-          else if (checkIfMultipleOfThree(i) && checkIfMultipleOfFive(i)) System.out.println("FizzBuzz");
-
-          else System.out.println(i);
-       }
-    }
-
-    private static boolean checkIfMultipleOfThree(int number) {
-        return number % 3 == 0;
-    }
-
-    private static boolean checkIfMultipleOfFive(int number) {
-        return number % 5 == 0;
+            else System.out.println(i);
+        }
     }
 }
